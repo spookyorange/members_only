147cf33af3a398d0dcf4978b7526ddd6f2441997
spookyorange <korkmazcoltu@mail.ru>
2021-12-30 18:05:11 +0300

    Project Finished

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+    @post = current_user.posts.create(post_params)
+
+    if @post.save
+     redirect_to root_path
+    else
+      render :new
+    end 
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
